62803bece38597e7edf1299dfb3429ae4c1b4837
Mahima1111 <mahimavijayan123@gmail.com>
2022-02-16 13:53:27 +0530

    CollegeApp

--- app/src/main/java/com/example/studentdbapp/DatabaseHelper.java
+++ app/src/main/java/com/example/studentdbapp/DatabaseHelper.java
@@ -7,12 +7,27 @@ import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.Nullable;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
-    public DatabaseHelper(Context context) {
-        super(context, name, factory, version);
+    static String DbName="College.db";
+    static String TableName="students";
+    static String Col1="id";
+    static String Col2="name";
+    static String Col3="rollno";
+    static String Col4="admno";
+    static String Col5="college";
+
+    public DatabaseHelper(Context context)
+    {
+        super(context, DbName, null,1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        String query="create table "+TableName+ "("+Col1+" Integer primary key autoincrement,"
+                +Col2+" text,"
+                +Col3+" text,"
+                +Col4+" text,"
+                +Col5+" text)";
+        db.execSQL(query);
 
     }
 
